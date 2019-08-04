534e1a23a1c244ec5b1b552378da953655fe2357
James Kirwa <jameskirwa34@gmail.com>
2019-08-04 19:04:12 +0300

    creating db

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export SECRET_KEY='james'
-export DATABASE_URL='postgresql+psycopg2://james:Uppercase2000@localhost/pitches'
+export DATABASE_URL='postgresql+psycopg2://james:james@localhost/pitches'
 export MAIL_USERNAME='jameskirwa34@gmail.com'
-export MAIL_PASSWORD='Uppercase2000'
+export MAIL_PASSWORD='james'
 
 
 python3.6 manage.py server
