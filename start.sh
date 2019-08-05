c7e471be1d98af922724dd7e7d27f7274f6dbeb5
James Kirwa <jameskirwa34@gmail.com>
2019-08-05 08:49:11 +0300

    change password

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export SECRET_KEY='james'
 export DATABASE_URL='postgresql+psycopg2://james:james@localhost/pitches'
 export MAIL_USERNAME='jameskirwa34@gmail.com'
-export MAIL_PASSWORD='james'
+export MAIL_PASSWORD='Uppercase95'
 
 
 python3.6 manage.py server
